59d5c588b1eee17a3370dfd9a585f49f467d3264
mohanavamsi0614 <mohanavamsi14@gmail.com>
2026-02-13 09:35:52 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import LandingPage from "./components/landingPage.jsx";
 import LoginPage from "./components/LoginPage.jsx";
 import AdminDashboard from "./components/AdminDashboard.jsx";
 import AdminTestManagement from "./components/LevelsManagement.jsx";
-import LevelsPage from "./components/LevelsPage.jsx";
 import TestTakingPage from "./components/testTakingPage.jsx";
 import ResultPage from "./components/ResultPage.jsx";
+import LevelsPage from "./components/LevelsPage.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useEffect, useState } from "react";
 
